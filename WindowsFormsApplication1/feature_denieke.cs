571942da27c47b6d97316897114a5c90b4d7b361
Denieke van de Velde <deniekevdvelde@gmail.com>
2017-08-07 15:47:44 +0200

    Denieke Feature added

---
+++ WindowsFormsApplication1/feature_denieke.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class feature_denieke
+    {
+        Random rnd = new Random(); //Generate random numbers
+        private int[] numbers;  //Hold random numbers
+
+        public string GenerateNumbers(int n)
+        {
+            string lNumbers = ""; //Message with the random numbers chosen
+            numbers = new int[n];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(1, 50);
+                lNumbers += numbers[i] + " ";
+            }
+           
+            return lNumbers;
+        }
+
+        public int getMax()
+        {
+            int max = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > max)
+                        max = numbers[i];
+                }
+           
+            return max;
+        }
+    }
+}
